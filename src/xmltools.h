4d6081a457376549a4762e230e4757823809c8a4
groman1 <sergeiroman060@gmail.com>
2026-08-20 10:58:18 +0300

    Add xmlToString() functionality

--- src/xmltools.h
+++ src/xmltools.h
@@ -8,16 +8,35 @@
 #define appendElement(ptr, value) insertElement(ptr, value, ptr->tagQty)
 #define initEmptyXML() parseXML("")
 
+// you can change this to char if you don't need wchars
+#define char_t wchar_t
+
+#if (char_t == wchar_t)
+#define string_str wcsstr
+#define string_cmp wcscmp
+#define string_cpy wcscpy
+#define string_ncpy wcsncpy
+#define string_len wcslen
+#define string_chr wcschr
+#else
+#define string_str strstr
+#define string_cmp strcmp
+#define string_cpy strcpy
+#define string_ncpy strncpy
+#define string_len strlen
+#define string_chr strchr
+#endif
+
 typedef struct {
-    wchar_t *name;
-    wchar_t *value;
+    char_t *name;
+    char_t *value;
 } xmlArg;
 
 typedef struct xmlStruct xml;
 typedef struct xmlTagStruct xmlTag;
 
 struct xmlTagStruct {
-    wchar_t *tagName;
+    char_t *tagName;
 	xml *child;
     xmlArg *args;
     uint32_t argsQty;
@@ -30,16 +49,15 @@ struct xmlStruct {
 	xmlTag *tagArr;
 };
 
-char *xmlToString(xml *ptr);
+char_t *xmlToString(xml *ptr, bool format);
 void fillEmptyXML(xml *ptr);
-xml *parseXML(wchar_t *string);
+xml *parseXML(char_t *string);
 void freeXML(xml *xmlDocument);
 void freeXMLValue(xml *ptr, uint32_t index);
-void copyElement(xml *ptr, xmlTag value, int position);
-int removeElement(xml *ptr, uint32_t index);
-int insertElement(xml *ptr, xmlTag value, uint32_t index);
-xmlTag *findElement(xml *ptr, wchar_t *text);
+void removeElement(xml *ptr, uint32_t index);
+void insertElement(xml *ptr, xmlTag value, uint32_t index);
+xmlTag *findElement(xml *ptr, char_t *text);
 void swapElements(xml *ptr, uint32_t index1, uint32_t index2);
-xml *nestElement(xml *ptr, int index);
+xml *nestElement(xml *ptr, uint32_t index);
 
 #endif
